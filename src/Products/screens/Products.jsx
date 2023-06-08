6745ae94b8c18a3112386a54a7fc08413edcde78
DeusEli <adonayvm32@gmail.com>
2023-06-08 03:24:17 -0600

    Add isLoading to Products

--- src/Products/screens/Products.jsx
+++ src/Products/screens/Products.jsx
@@ -3,10 +3,12 @@ import { AddProduct } from "../../components/modal/AddProduct";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 import { api } from "../../api/api";
+import Oval from "react-loading-icons/dist/esm/components/oval";
 
 export const Products = () => {
   const [products, setProducts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const loadProducts = async () => {
     const { data } = await api.get("products");
@@ -15,6 +17,7 @@ export const Products = () => {
 
   useEffect(() => {
     loadProducts();
+    setIsLoading(false);
   }, []);
   return (
     <div className="flex flex-col items-center justify-center">
@@ -33,59 +36,62 @@ export const Products = () => {
             </tr>
           </thead>
           <tbody>
-            {products.map((category) => (
-              <>
-                {/* row 1 */}
-                <tr className="hover">
-                  <th>
-                    <label>
-                      <input type="checkbox" className="checkbox" />
-                    </label>
-                  </th>
-                  <td>
-                    <div className="flex items-center space-x-3">
-                      <div className="avatar">
-                        <div className="mask mask-squircle w-24 h-24 rounded-full">
-                          <img
-                            src= {category?.image}
-                            alt="Producto"
-                          />
+            {isLoading ? (
+              <Oval stroke="#0d6efd" />
+            ) : (
+              products.map((category) => (
+                <>
+                  {/* row 1 */}
+                  <tr className="hover">
+                    <th>
+                      <label>
+                        <input type="checkbox" className="checkbox" />
+                      </label>
+                    </th>
+                    <td>
+                      <div className="flex items-center space-x-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-24 h-24 rounded-full">
+                            <img src={category?.image} alt="Producto" />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="font-bold">{category?.name}</div>
+                          <div className="text-sm opacity-50">
+                            $ {category?.price}
+                          </div>
                         </div>
                       </div>
-                      <div>
-                        <div className="font-bold">{category?.name}</div>
-                        <div className="text-sm opacity-50">$ {category?.price}</div>
+                    </td>
+                    <td>
+                      <div className="py-1 px-2 bg-green-100 text-xs font-bold text-green-800 rounded-lg flex justify-center">
+                        {category?.stock}
                       </div>
-                    </div>
-                  </td>
-                  <td>
-                    <div className="py-1 px-2 bg-green-100 text-xs font-bold text-green-800 rounded-lg flex justify-center">
-                      {category?.stock}
-                    </div>
-                  </td>
-                  <td>{category?.description}</td>
-                  <td>
-                    <div className="py-1 px-2 bg-gray-200 text-xs font-bold rounded-lg flex justify-center">
-                      {category?.category.name}
-                    </div>
-                  </td>
-                  <th>
-                    <button className="btn btn-ghost btn-xs">
-                      <div className="w-6 h-6">
-                        <img src="/src/images/editar.png" />
+                    </td>
+                    <td>{category?.description}</td>
+                    <td>
+                      <div className="py-1 px-2 bg-gray-200 text-xs font-bold rounded-lg flex justify-center">
+                        {category?.category.name}
                       </div>
-                    </button>
-                  </th>
-                  <th>
-                    <button className="btn btn-ghost btn-xs">
-                      <div className="w-6 h-6">
-                        <img src="/src/images/trash.png" />
-                      </div>
-                    </button>
-                  </th>
-                </tr>
-              </>
-            ))}
+                    </td>
+                    <th>
+                      <button className="btn btn-ghost btn-xs">
+                        <div className="w-6 h-6">
+                          <img src="/src/images/editar.png" />
+                        </div>
+                      </button>
+                    </th>
+                    <th>
+                      <button className="btn btn-ghost btn-xs">
+                        <div className="w-6 h-6">
+                          <img src="/src/images/trash.png" />
+                        </div>
+                      </button>
+                    </th>
+                  </tr>
+                </>
+              ))
+            )}
           </tbody>
         </table>
       </div>
