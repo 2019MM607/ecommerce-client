5f0d5042e45b53c89633553b4415ea6e80c8f231
Kevin <kjlmenjivar@gmail.com>
2023-06-08 03:05:45 -0600

    [ADD] Method GET done

--- src/Categories/screens/Categories.jsx
+++ src/Categories/screens/Categories.jsx
@@ -1,131 +1,63 @@
+import { useEffect, useState } from "react";
+import { api } from "../../api/api";
+
 export const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  const loadCategories = async () => {
+    const { data } = await api.get("categories");
+    data ? setCategories(data) : setCategories([]);
+  };
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
   return (
     <div className="w-full h-auto flex flex-col justify-center">
       {/* Form */}
       <div className="w-auto h-auto py-5 px-10 flex flex-col justify-center">
-        <div className="text-lg font-bold">
-          Nueva Categoría
-        </div>
+        <div className="text-lg font-bold">Nueva Categoría</div>
         <form className="" action="POST">
           <div className="flex justify-left items-center">
             <label className="mr-3">Nombre de la Categoría</label>
-            <input type="text" className="bg-gray-100 px-3 py-1 rounded-md focus:outline-none mr-3"/>
-            <input type="submit" className="py-1 px-3 bg-blue-900 rounded-md text-white font-semibold" value="Agregar"/>
+            <input
+              type="text"
+              className="bg-gray-100 px-3 py-1 rounded-md focus:outline-none mr-3"
+            />
+            <input
+              type="submit"
+              className="py-1 px-3 bg-blue-900 rounded-md text-white font-semibold"
+              value="Agregar"
+            />
           </div>
         </form>
       </div>
       {/* Cards */}
       <div className="w-auto h-auto py-5 px-10 flex justify-center items-center grid grid-cols-5 gap-4">
         {/* Card */}
-      <div className="w-30 h-auto p-3 flex flex-col justify-center items-center rounded-xl shadow-xl">
-        {/* Titulo */}
-        <div className="py-2 px-10">
-          <div className="text-md font-bold">Lacteos</div>
-        </div>
-        
-        <div className="flex flex-row justify-center items-center">
-          {/* Editar */}
-          <div className="py-1 px-3 flex justify-center items-center bg-neutral rounded-lg mr-2">
-            <div className="text-xs text-white">Editar</div>
+        {categories.map((category) => (
+          <>
+          <div className="w-30 h-auto p-3 flex flex-col justify-center items-center rounded-xl shadow-xl">
+            {/* Titulo */}
+            <div className="py-2 px-10">
+              <div className="text-md font-bold">{category?.name}</div>
+            </div>
+
+            <div className="flex flex-row justify-center items-center">
+              {/* Editar */}
+              <div className="py-1 px-3 flex justify-center items-center bg-neutral rounded-lg mr-2">
+                <div className="text-xs text-white">Editar</div>
+              </div>
+              {/* Eliminar */}
+              <div className="py-1 px-3 flex justify-center items-center bg-accent rounded-lg">
+                <div className="text-xs text-white">Eliminar</div>
+              </div>
+            </div>
           </div>
-          {/* Eliminar */}
-          <div className="py-1 px-3 flex justify-center items-center bg-accent rounded-lg">
-            <div className="text-xs text-white">Eliminar</div>
-          </div>
-        </div>
+          </>
+        ))}
       </div>
-      {/* Card */}
-      <div className="w-30 h-auto p-3 flex flex-col justify-center items-center rounded-xl shadow-xl">
-        {/* Titulo */}
-        <div className="py-2 px-10">
-          <div className="text-md font-bold">Lacteos</div>
-        </div>
-        
-        <div className="flex flex-row justify-center items-center">
-          {/* Editar */}
-          <div className="py-1 px-3 flex justify-center items-center bg-neutral rounded-lg mr-2">
-            <div className="text-xs text-white">Editar</div>
-          </div>
-          {/* Eliminar */}
-          <div className="py-1 px-3 flex justify-center items-center bg-accent rounded-lg">
-            <div className="text-xs text-white">Eliminar</div>
-          </div>
-        </div>
-      </div>
-      {/* Card */}
-      <div className="w-30 h-auto p-3 flex flex-col justify-center items-center rounded-xl shadow-xl">
-        {/* Titulo */}
-        <div className="py-2 px-10">
-          <div className="text-md font-bold">Lacteos</div>
-        </div>
-        
-        <div className="flex flex-row justify-center items-center">
-          {/* Editar */}
-          <div className="py-1 px-3 flex justify-center items-center bg-neutral rounded-lg mr-2">
-            <div className="text-xs text-white">Editar</div>
-          </div>
-          {/* Eliminar */}
-          <div className="py-1 px-3 flex justify-center items-center bg-accent rounded-lg">
-            <div className="text-xs text-white">Eliminar</div>
-          </div>
-        </div>
-      </div>
-      {/* Card */}
-      <div className="w-30 h-auto p-3 flex flex-col justify-center items-center rounded-xl shadow-xl">
-        {/* Titulo */}
-        <div className="py-2 px-10">
-          <div className="text-md font-bold">Lacteos</div>
-        </div>
-        
-        <div className="flex flex-row justify-center items-center">
-          {/* Editar */}
-          <div className="py-1 px-3 flex justify-center items-center bg-neutral rounded-lg mr-2">
-            <div className="text-xs text-white">Editar</div>
-          </div>
-          {/* Eliminar */}
-          <div className="py-1 px-3 flex justify-center items-center bg-accent rounded-lg">
-            <div className="text-xs text-white">Eliminar</div>
-          </div>
-        </div>
-      </div>
-      {/* Card */}
-      <div className="w-30 h-auto p-3 flex flex-col justify-center items-center rounded-xl shadow-xl">
-        {/* Titulo */}
-        <div className="py-2 px-10">
-          <div className="text-md font-bold">Lacteos</div>
-        </div>
-        
-        <div className="flex flex-row justify-center items-center">
-          {/* Editar */}
-          <div className="py-1 px-3 flex justify-center items-center bg-neutral rounded-lg mr-2">
-            <div className="text-xs text-white">Editar</div>
-          </div>
-          {/* Eliminar */}
-          <div className="py-1 px-3 flex justify-center items-center bg-accent rounded-lg">
-            <div className="text-xs text-white">Eliminar</div>
-          </div>
-        </div>
-      </div>
-      {/* Card */}
-      <div className="w-30 h-auto p-3 flex flex-col justify-center items-center rounded-xl shadow-xl">
-        {/* Titulo */}
-        <div className="py-2 px-10">
-          <div className="text-md font-bold">Lacteos</div>
-        </div>
-        
-        <div className="flex flex-row justify-center items-center">
-          {/* Editar */}
-          <div className="py-1 px-3 flex justify-center items-center bg-neutral rounded-lg mr-2">
-            <div className="text-xs text-white">Editar</div>
-          </div>
-          {/* Eliminar */}
-          <div className="py-1 px-3 flex justify-center items-center bg-accent rounded-lg">
-            <div className="text-xs text-white">Eliminar</div>
-          </div>
-        </div>
-      </div>
-      </div>
-      
     </div>
   );
 };
--- src/Products/screens/Products.jsx
+++ src/Products/screens/Products.jsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AddProduct } from "../../components/modal/AddProduct";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
+import { api } from "../../api/api";
 
 export const Products = () => {
+  const [products, setProducts] = useState([]);
   const [open, setOpen] = useState(false);
+
+  const loadProducts = async () => {
+    const { data } = await api.get("products");
+    data ? setProducts(data) : setProducts([]);
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="w-3/4 m-12">
@@ -22,145 +33,59 @@ export const Products = () => {
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
-            <tr className="hover">
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-24 h-24 rounded-full">
-                      <img
-                        src="https://images.pexels.com/photos/4033112/pexels-photo-4033112.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                        alt="Producto"
-                      />
+            {products.map((category) => (
+              <>
+                {/* row 1 */}
+                <tr className="hover">
+                  <th>
+                    <label>
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </th>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-24 h-24 rounded-full">
+                          <img
+                            src= {category?.image}
+                            alt="Producto"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{category?.name}</div>
+                        <div className="text-sm opacity-50">$ {category?.price}</div>
+                      </div>
                     </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Tomatoes</div>
-                    <div className="text-sm opacity-50">$0.99 / lb</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <div className="py-1 px-2 bg-green-100 text-xs font-bold text-green-800 rounded-lg flex justify-center">
-                  25 lbs
-                </div>
-              </td>
-              <td>Some tomatoes used as an example</td>
-              <td>
-                <div className="py-1 px-2 bg-gray-200 text-xs font-bold rounded-lg flex justify-center">
-                  Vegetales
-                </div>
-              </td>
-              <th>
-                <button className="btn btn-ghost btn-xs">
-                  <div className="w-6 h-6">
-                    <img src="/src/images/editar.png" />
-                  </div>
-                </button>
-              </th>
-              <th>
-                <button className="btn btn-ghost btn-xs">
-                  <div className="w-6 h-6">
-                    <img src="/src/images/trash.png" />
-                  </div>
-                </button>
-              </th>
-            </tr>
-            {/* row 2 */}
-            <tr className="hover">
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-24 h-24 rounded-full">
-                      <img
-                        src="https://images.pexels.com/photos/4197905/pexels-photo-4197905.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                        alt="Producto"
-                      />
+                  </td>
+                  <td>
+                    <div className="py-1 px-2 bg-green-100 text-xs font-bold text-green-800 rounded-lg flex justify-center">
+                      {category?.stock}
                     </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Bread</div>
-                    <div className="text-sm opacity-50">$0.45 / unidad</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                25
-                <br />
-                <span className="badge badge-ghost badge-sm">unidades</span>
-              </td>
-              <td>Bread used as an example</td>
-              <th>Panadería</th>
-              <th>
-                <button className="btn btn-ghost btn-xs">
-                  <div className="w-6 h-6">
-                    <img src="/src/images/editar.png" />
-                  </div>
-                </button>
-              </th>
-              <th>
-                <button className="btn btn-ghost btn-xs">
-                  <div className="w-6 h-6">
-                    <img src="/src/images/trash.png" />
-                  </div>
-                </button>
-              </th>
-            </tr>
-            {/* row 3 */}
-            <tr className="hover">
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-24 h-24 rounded-full">
-                      <img
-                        src="https://images.pexels.com/photos/2985167/pexels-photo-2985167.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                        alt="Producto"
-                      />
+                  </td>
+                  <td>{category?.description}</td>
+                  <td>
+                    <div className="py-1 px-2 bg-gray-200 text-xs font-bold rounded-lg flex justify-center">
+                      {category?.category.name}
                     </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Eggs</div>
-                    <div className="text-sm opacity-50">$0.30 / unidad</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                80
-                <br />
-                <span className="badge badge-ghost badge-sm">unidades</span>
-              </td>
-              <td>Some eggs used as an example</td>
-              <th>Lácteos</th>
-              <th>
-                <button className="btn btn-ghost btn-xs">
-                  <div className="w-6 h-6">
-                    <img src="/src/images/editar.png" />
-                  </div>
-                </button>
-              </th>
-              <th>
-                <button className="btn btn-ghost btn-xs">
-                  <div className="w-6 h-6">
-                    <img src="/src/images/trash.png" />
-                  </div>
-                </button>
-              </th>
-            </tr>
+                  </td>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">
+                      <div className="w-6 h-6">
+                        <img src="/src/images/editar.png" />
+                      </div>
+                    </button>
+                  </th>
+                  <th>
+                    <button className="btn btn-ghost btn-xs">
+                      <div className="w-6 h-6">
+                        <img src="/src/images/trash.png" />
+                      </div>
+                    </button>
+                  </th>
+                </tr>
+              </>
+            ))}
           </tbody>
         </table>
       </div>
